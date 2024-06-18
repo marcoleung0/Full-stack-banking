f1826211ed08fa7b5d597118fefa83e992c223e5
marcoleung0 <162746481+marcoleung0@users.noreply.github.com>
2024-06-18 05:55:22 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	renamed:    server/components/Account.js -> components/Account.js
    	new file:   components/AccountForm.js
    	new file:   components/AddAccountForm.js
    	renamed:    server/components/Summary.js -> components/Summary.js
    	new file:   server/account.js
    	deleted:    server/components/AccountForm.js
    	new file:   server/index.js
    	new file:   server/src/actions/account.js
    	new file:   server/src/reducers/account.js
    	new file:   server/src/utils/mask.js

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const authRoute = require('./routes/auth');
+const profileRoute = require('./routes/profile');
+const accountRoute = require('./routes/account');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use(authRoute);
+app.use(accountRoute.Router);
+app.use(profileRoute);
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
